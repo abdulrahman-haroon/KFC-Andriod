0bf0ede65191e55893acce799ba193b77621b0b8
Abdul Rahman Haroon <abdulrahmanharoon18@gmail.com>
2021-01-01 22:15:18 +0500

    Upgraded the navigation drawer and added bucket

--- KFC/app/src/main/java/com/example/kfc/BucketActivity.java
+++ KFC/app/src/main/java/com/example/kfc/BucketActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,20 +14,20 @@ import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MoreActivity extends AppCompatActivity {
+public class BucketActivity extends AppCompatActivity {
     NavigationView nav;
     ActionBarDrawerToggle toggle;
     DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_more);
+        setContentView(R.layout.activity_bucket);
 
-        Toolbar toolbar = findViewById(R.id.more_toolbar);
+        Toolbar toolbar = findViewById(R.id.bucket_toolbar);
         setSupportActionBar(toolbar);//setting the Action bar of this activity.
 
-        nav=findViewById(R.id.navigation_menu);
-        drawerLayout=findViewById(R.id.drawer_more);
+        nav=findViewById(R.id.navigation_menu_bucket);
+        drawerLayout=findViewById(R.id.drawer_bucket);
 
         toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_nav,R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
@@ -46,11 +45,11 @@ public class MoreActivity extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(), "Menu", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.nav_bucket:
-                        intent=new Intent(getApplicationContext(),BucketActivity.class);
+                         intent=new Intent(getApplicationContext(),BucketActivity.class);
                         startActivity(intent);
                         break;
                     case R.id.nav_more:
-                        intent=new Intent(getApplicationContext(),MoreActivity.class);
+                         intent=new Intent(getApplicationContext(),MoreActivity.class);
                         startActivity(intent);
                         break;
                     case R.id.nav_logout:
@@ -62,33 +61,7 @@ public class MoreActivity extends AppCompatActivity {
             }
         });
     }
-    public void onClickStore(View view){
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kfcpakistan.com/page/location"));
-        startActivity(browserIntent);
+    public void onClickStartOrder(View view){
+        Toast.makeText(this, "Goes to MENU", Toast.LENGTH_SHORT).show();
     }
-    public void onClickContactUs(View view){
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kfcpakistan.com/page/contact"));
-        startActivity(browserIntent);
-    }
-    public void onClickAboutUs(View view){
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kfcpakistan.com/page/about-us"));
-        startActivity(browserIntent);
-    }
-    public void onClickMitao(View view){
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kfcpakistan.com/page/mitao-bhook"));
-        startActivity(browserIntent);
-    }
-    public void onClickSecret(View view){
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kfcpakistan.com/page/nutritional-information"));
-        startActivity(browserIntent);
-    }
-    public void onClickPrivacy(View view){
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kfcpakistan.com/page/privacy-policy"));
-        startActivity(browserIntent);
-    }
-    public void onClickTerms(View view){
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kfcpakistan.com/page/terms-and-conditions"));
-        startActivity(browserIntent);
-    }
-
 }
