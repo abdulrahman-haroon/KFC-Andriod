2f99cc6cd7ad7fc45fe1a19b000f0c52c23022bb
Iqra Ahmed <iolazola1622@gmail.com>
2021-01-02 00:08:51 +0500

    HomePage completed

--- KFC/app/src/main/java/com/example/kfc/Promotion_Deals.java
+++ KFC/app/src/main/java/com/example/kfc/Promotion_Deals.java
@@ -1,9 +1,12 @@
 package com.example.kfc;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,13 +15,20 @@ public class Promotion_Deals extends AppCompatActivity {
 
     ImageView imageView;
     CardView cardView;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promotion__deals);
 
+        Toolbar toolbar=findViewById(R.id.pro_toolbar);
+        setSupportActionBar(toolbar);
+
 
     }
+
+
 
 
     public void ClickPromotionOne(View view) {
